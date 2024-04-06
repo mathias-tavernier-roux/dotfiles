935e42a77fa7e1cfc90712b1b39a6f9f6ab97d73
Pikatsuto <pikatsuto@gmail.com>
2024-04-07 01:20:34 +0200

    feat: refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     ## ------------------------------------------------------------- ##
-    hosts.url = github:StevenBlack/hosts;
+    hosts.url = "github:StevenBlack/hosts";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     ## ------------------------------------------------------------- ##
     home-manager = {
@@ -30,72 +30,83 @@
     system = "x86_64-linux";
     hostname = "NixAchu";
 
-    computer = {
-      hostname = "${hostname}-Fix";
-      vm = {
-        cores = 4;
-        threads = 2;
-        memory = 12;
-        diskSize = 512;
-        diskPath = "/var/lib/libvirt/images";
-        restartDm = false;
-        videoVirtio = false;
-        pcies = [
-          {
-            pcie = {
-              vmBus = "09";
-              bus = "01";
-              slot = "00";
-              function = "0";
-            };
-            driver = ''nouveau'';
-            blacklistDriver = true;
-          }
-          {
-            pcie = {
-              vmBus = "09";
-              bus = "01";
-              slot = "00";
-              function = "1";
-            };
-            driver = ''nouveau'';
-            blacklistDriver = true;
-          }
-          {
-            pcie = {
-              vmBus = "09";
-              bus = "01";
-              slot = "00";
-              function = "2";
-            };
-            driver = ''nouveau'';
-            blacklistDriver = true;
-          }
-          {
-            pcie = {
-              vmBus = "09";
-              bus = "01";
-              slot = "00";
-              function = "3";
-            };
-            driver = ''nouveau'';
-            blacklistDriver = true;
-          }
-        ];
+    applyAttrNames = builtins.mapAttrs (name: f: f name);
+
+    computers = applyAttrNames {
+      "${hostname}-Fix" = self: {
+        hostname = "${self}";
+        vm = {
+          cores = 4;
+          threads = 2;
+          memory = 12;
+          diskSize = 512;
+          diskPath = "/var/lib/libvirt/images";
+          restartDm = false;
+          videoVirtio = false;
+          pcies = [
+            {
+              pcie = {
+                vmBus = "09";
+                bus = "01";
+                slot = "00";
+                function = "0";
+              };
+              driver = ''nouveau'';
+              blacklistDriver = true;
+            }
+            {
+              pcie = {
+                vmBus = "09";
+                bus = "01";
+                slot = "00";
+                function = "1";
+              };
+              driver = ''nouveau'';
+              blacklistDriver = true;
+            }
+            {
+              pcie = {
+                vmBus = "09";
+                bus = "01";
+                slot = "00";
+                function = "2";
+              };
+              driver = ''nouveau'';
+              blacklistDriver = true;
+            }
+            {
+              pcie = {
+                vmBus = "09";
+                bus = "01";
+                slot = "00";
+                function = "3";
+              };
+              driver = ''nouveau'';
+              blacklistDriver = true;
+            }
+          ];
+        };
+        modules = [];
       };
-    };
-    ## ------------------------------------------------------------- ## 
-    laptop = {
-      hostname = "${hostname}-Lap";
-      vm = {
-        cores = 2;
-        threads = 2;
-        memory = 8;
-        diskSize = 128;
-        diskPath = "/home/${username}/VM/Disk";
-        restartDm = false;
-        videoVirtio = true;
-        pcies = false;
+      ### --------------------------------------------------------- ### 
+      "${hostname}-Lap" = self: {
+        hostname = "${self}";
+        vm = {
+          cores = 2;
+          threads = 2;
+          memory = 8;
+          diskSize = 128;
+          diskPath = "/home/${username}/VM/Disk";
+          restartDm = false;
+          videoVirtio = true;
+          pcies = false;
+        };
+        modules = [
+          nixos-hardware.nixosModules.asus-battery
+          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-pc
+          nixos-hardware.nixosModules.common-pc-ssd
+        ];
       };
     };
     ## ------------------------------------------------------------- ##
@@ -114,33 +125,17 @@
 #######################################################################
   in
   {
-    nixosConfigurations = {
-      ${computer.hostname} = nixpkgs.lib.nixosSystem {
-        inherit system;
-        #### ----------------------------------------------------- ####
-        modules = default_modules ++ [
-          (import ./computer.nix {
-            inherit computer username home-manager;
-          })
-        ];
-      };
-      ### --------------------------------------------------------- ###
-      ${laptop.hostname} = nixpkgs.lib.nixosSystem {
-        inherit system;
-        #### ----------------------------------------------------- ####
-        modules = default_modules ++ [
-          (import ./computer.nix {
-            computer = laptop;
-            inherit username home-manager;
-          })
-          ##### ------------------------------------------------- #####
-          nixos-hardware.nixosModules.asus-battery
-          nixos-hardware.nixosModules.common-cpu-intel
-          nixos-hardware.nixosModules.common-pc
-          nixos-hardware.nixosModules.common-pc-ssd
-        ];
-      };
-    };
+    nixosConfigurations = (nixpkgs.lib.genAttrs (builtins.attrNames computers)
+    (name: nixpkgs.lib.nixosSystem {
+      inherit system;
+      #### ----------------------------------------------------- ####
+      modules = default_modules ++ computers."${name}".modules ++ [
+        (import ./computer.nix {
+          computer = computers."${name}";
+          inherit username home-manager;
+        })
+      ];
+    }));
   };
 #######################################################################
 }
