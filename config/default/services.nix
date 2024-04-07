929dbbe4319a856cef615d595eeb1080cb338c89
Pikatsuto <pikatsuto@gmail.com>
2024-04-07 19:52:16 +0200

    feat: add tlp for battery

--- config/default/services.nix
+++ config/default/services.nix
@@ -84,6 +84,16 @@
     };
     ## -------------------------------------------------------------- ##
     upower.enable = true;
+    power-profiles-daemon.enable = false;
+    thermald.enable = true;
+    tlp = {
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      };
+    };
   };
   # ------------------------------------------------------------------ #
   systemd.targets = {
