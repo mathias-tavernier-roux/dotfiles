82c879e359d4820b0f3cbcc74d477eca52199266
Pikatsuto <pikatsuto@gmail.com>
2024-05-29 13:31:25 +0200

    fix: default app

--- home/default/programs.nix
+++ home/default/programs.nix
@@ -5,6 +5,51 @@
 #######################################################################
   nixpkgs.config.allowUnfree = true;
   # ----------------------------------------------------------------- #
+  xdg.mimeApps = {
+    enable = true;
+    ## ------------------------------------------------------------- ##
+    defaultApplications = {
+      "application/zip" = [
+       "xarchiver.desktop"
+       "unzip"
+      ];
+      "text/plain" = [
+        "code.desktop"
+        "neovim.desktop"
+      ];
+      "application/pdf" = [
+        "qpdfview.desktop"
+        "firefox.desktop"
+      ];
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" =
+        "libreoffice.desktop";
+      "application/vnd.oasis.opendocument.text" =
+        "libreoffice.desktop";
+      "image/png" = [
+        "viewnior.desktop"
+        "feh.desktop"
+      ];
+      "image/jpg" = [
+        "viewnior.desktop"
+        "feh.desktop"
+      ];
+      "image/jpeg" = [
+        "viewnior.desktop"
+        "feh.desktop"
+      ];
+      "image/gif" = [
+        "viewnior.desktop"
+        "feh.desktop"
+      ];
+      "image/webm" = [
+        "viewnior.desktop"
+        "feh.desktop"
+      ];
+      "application/vnd.microsoft.portable-executable" =
+        "wine";
+    };
+  };
+  # ----------------------------------------------------------------- #
   programs = {
     home-manager.enable = true;
     ## ------------------------------------------------------------- ##
