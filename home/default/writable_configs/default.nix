fa454924264863ef35e3ae7dc5bab63fa1fbc95d
Pikatsuto <pikatsuto@gmail.com>
2024-06-07 17:34:48 +0200

    feat: simplify cpwrconf

--- home/default/writable_configs/default.nix
+++ home/default/writable_configs/default.nix
@@ -1,24 +1,12 @@
-{ stdenv, lib }:
-############
-# Packages #
+{ ... }:
+{
+#########
+# Files #
 #######################################################################
-stdenv.mkDerivation (finalAttrs: {
-  pname = "Writable-configs";
-  version = "unstable-2023-07-16";
-  # ----------------------------------------------------------------- #
-  src = ./src;
-  # ----------------------------------------------------------------- #
-  installPhase = ''
-    runHook preInstall
-    install -D --target-directory=$out/bin/ ./cpwrconf
-    runHook postInstall
-  '';
-  # ----------------------------------------------------------------- #
-  meta = with lib; {
-    description = "copy config with writable for skip programs error";
-    maintainers = [ maintainers.pikatsuto ];
-    licenses = licenses.lgpl;
-    platforms = platforms.linux;
+  home.file.lockscreen_configs = {
+    source = ./cpwrconf;
+    target = ".local/bin/cpwrconf";
+    recursive = false;
   };
 #######################################################################
-})
+}
